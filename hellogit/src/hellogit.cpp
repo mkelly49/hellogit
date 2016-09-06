e096caa3e2cc623f405bd12484cc2d4842c71645
Michael A. Kelly <mkelly15@jhu.edu>
2016-09-06 19:54:07 -0400

    Initial commit.

---
+++ hellogit/src/hellogit.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : hellogit.cpp
+// Author      : Michael A. Kelly
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	auto str = "Hello, world!";
+	cout << str << endl;
+	return 0;
+}
